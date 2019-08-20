fd70be65a7166be4ba3381229daa8f800f9faedc
MARK SHIN <home@MARKs-MacBook-Pro.local>
2019-08-19 21:50:04 -0400

    Second Commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+    mode: process.env.NODE_ENV,
+    entry: "./client/index.js",
+    devServer: {
+        port:8080,
+        publicPath: '/build/'
+    },
+    output: {
+        path: path.join(__dirname, '/build'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }               
+                }
+            }
+        ]
+    }
+}
